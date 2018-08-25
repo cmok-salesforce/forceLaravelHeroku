a5c33983ec9c46ecbfecf28fbec9de40c1d2ec85
mokchenda <mokchend@yahoo.com>
2018-08-26 01:36:48 +0200

    readDirectory

--- app/Http/Controllers/MyDummyController.php
+++ app/Http/Controllers/MyDummyController.php
@@ -3,24 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Helpers\readDirectory;
 
 class MyDummyController extends Controller
 {
     public function index()
     {
-        print('<h1>MyDummyController</h1>');
-        // INITIAL XML 
-        //
-        // THE SKELETON OF XML THAT WE WILL ADD THE DIRECTORY'S CONTENTS TO
-        /*
-        $xml_string = <<< XML
-        <?xml version='1.0' encoding='UTF-8'?>
-        <files>
-        </files>
-        XML;
-        $xml_generator = new SimpleXMLElement($xml_string);
-        
-        Helpers::readDirectory('.', $xml_generator); 
-        dd($xml_generator->asXML()); */
+        $xml_string = "<files></files>";
+        $xml_generator = new \SimpleXMLElement($xml_string);
+        readDirectory('.', $xml_generator); 
+        dd($xml_generator->asXML()); 
     }    
 }
